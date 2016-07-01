ec444d3a5fbcc409cb2d3339a33075491ec9782e
refluster <refluster@gmail.com>
2016-07-01 23:47:07 +0900

    drawable w/ Furniture class

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const WH_RATIO = (3/4);
 
-var Furniture = function() {
+var Furniture = function(width, height) {
+	this.width = width;
+	this.height = height;
+
 	// create items
 	this.item = [];
 	this.item.push(new Item(Item.CIRCLE, 40));
@@ -8,9 +11,10 @@ var Furniture = function() {
 	this.item.push(new Item(Item.SQUARE, 40));
 	for (var i = 0; i < this.item.length; i++) {
 		// initialize position randomely in the canvas
-		var x = parseInt(Math.random()*(this.canvasWidth - 40) + 20);
-		var y = parseInt(Math.random()*(this.canvasHeight - 40) + 20);
+		var x = parseInt(Math.random()*(this.width - 40) + 20);
+		var y = parseInt(Math.random()*(this.height - 40) + 20);
 		this.item[i].setPosition(x, y);
+		console.log({x: x, y: y});
 	}
 };
 
@@ -29,10 +33,10 @@ Furniture.prototype.checkItem = function(x, y) {
 	return null;
 };
 
-Furniture.prototype.move = function(x, y) {
-	if (x != this.item[this.dragItem].x ||
-		y != this.item[this.dragItem].y) {
-		this.item[this.dragItem].setPosition(x, y);
+Furniture.prototype.move = function(idx, x, y) {
+	if (x != this.item[idx].x ||
+		y != this.item[idx].y) {
+		this.item[idx].setPosition(x, y);
 		return true;
 	}
 	return false;
@@ -52,14 +56,22 @@ var Apl = function() {
 	// resize canvas
 	$canvas.css('width', $canvas.width());
 	$canvas.css('height', $canvas.width()*WH_RATIO);
+	this.canvasStyleWidth = $canvas.width();
+	this.canvasStyleHeight = $canvas.height();
+
 	$canvas.attr('width', $canvas.width());
 	$canvas.attr('height', $canvas.width()*WH_RATIO);
 
 	// get canvas info
 	this.canvasLeft = $canvas.offset().left;
 	this.canvasTop = $canvas.offset().top;
-	this.canvasWidth = $canvas.attr('width')
-	this.canvasHeight = $canvas.attr('height')
+	this.canvasWidth = parseInt($canvas.attr('width'));
+	this.canvasHeight = parseInt($canvas.attr('height'));
+
+	console.log({cw: this.canvasStyleWidth,
+				 ch: this.canvasStyleHeight,
+				 w: this.canvasWidth,
+				 h: this.canvasHeight});
 
 	// context settnigs
 	this.ctx.strokeStyle = "#888";
@@ -67,7 +79,7 @@ var Apl = function() {
 	this.ctx.globalCompositeOperation = "source-over";
 
 	// create room
-	this.furniture = new Furniture();
+	this.furniture = new Furniture(this.canvasWidth, this.canvasHeight);
 
 	// set events to the canvas
 	$canvas.mousedown(this.hDown.bind(this));
@@ -87,15 +99,23 @@ Apl.prototype.draw = function() {
 	this.ctx.restore();
 };
 Apl.prototype.checkItem = function(x, y) {
-	this.furniture.checkItem(x, y);
+	x *= this.canvasWidth / this.canvasStyleWidth;
+	y *= this.canvasHeight / this.canvasStyleHeight;
+
+	return this.furniture.checkItem(x, y);
 };
 Apl.prototype.hDown = function(evt) {
 	if (!this.dragging) {
 		// convert coordinate from point to canvas
 		var x = parseInt(evt.pageX - this.canvasLeft);
 		var y = parseInt(evt.pageY - this.canvasTop);
+		console.log({x: x, y: y});
+		x *= this.canvasWidth / this.canvasStyleWidth;
+		y *= this.canvasHeight / this.canvasStyleHeight;
+		console.log({x: x, y: y});
 		// check if any object is at the point
 		var itemIdx = this.checkItem(x, y);
+		console.log(itemIdx);
 		if (itemIdx != null) {
 			this.dragging = true;
 			this.dragItem = itemIdx;
@@ -123,8 +143,10 @@ Apl.prototype.hMove = function(evt) {
 		// convert coordinate from point to canvas
 		var x = parseInt(evt.pageX - this.canvasLeft);
 		var y = parseInt(evt.pageY - this.canvasTop);
+		x *= this.canvasWidth / this.canvasStyleWidth;
+		y *= this.canvasHeight / this.canvasStyleHeight;
 		// check if the canvas should be updated
-		if (this.furniture.move(x, y)) {
+		if (this.furniture.move(this.dragItem, x, y)) {
 			this.draw();
 		}
 	}
