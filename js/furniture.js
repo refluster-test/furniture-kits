3bb9145e3634ec0428c08c4aa293a2944970a1dc
refluster <refluster@gmail.com>
2016-07-07 00:02:49 +0900

    refactor by removing canvas operation

--- js/furniture.js
+++ js/furniture.js
@@ -1,93 +1,3 @@
-var Furniture = function(left, top, width, height) {
-	Element.call(this, left, top, width, height);
-
-	this.state = new Database().restore();
-
-	// create items
-	this.item = [];
-	for (var i = 0; i < this.state.item.length; i++) {
-		var item = new this.state.item[i].type();
-		item.setPosition(this.state.item[i].pos);
-		this.addItem(item);
-	}
-
-	this.dragItem = undefined;
-};
-
-Object.setPrototypeOf(Furniture.prototype, Element.prototype);
-
-Furniture.prototype.addItem = function(item, svgItem) {
-	item.setConfig(this.state.config);
-
-	switch(item.linkType) {
-	case Item.LINK_VARIABLE_HORIZONTAL:
-		for (var j = 0; j < this.item.length; j++) {
-			if (this.item[j].type === ItemFrame) {
-				item.linkItem[0] = this.item[j];
-			}
-			if (this.item[j].type === ItemFrame) {
-				item.linkItem[1] = this.item[j];
-			}
-		}
-		break;
-	case Item.LINK_VARIABLE_VERTICAL:
-		break;
-	case Item.LINK_ATTACH_HORIZONTAL:
-		break;
-	case Item.LINK_ATTACH_TOP:
-		break;
-	}
-	item.calcArea();
-
-	this.item.push(item);
-};
-
-Furniture.prototype.delItem = function(idx) {
-	this.state.item.splice(idx, 1);
-	this.item.splice(idx, 1);
-};
-
-Furniture.prototype.draw = function(ctx) {
-	for (var i = 0; i < this.item.length; i++) {
-		this.item[i].draw(ctx);
-	}
-};
-
-Furniture.prototype.svgDraw = function(svg) {
-	for (var i = 0; i < this.item.length; i++) {
-		this.svgItem[i].draw(svg);
-	}
-};
-
-Furniture.prototype.grabItem = function(x, y) {
-	for (var i = 0; i < this.item.length; i++) {
-		if (this.item[i].isInternal(x, y)) {
-			this.dragItem = this.item[i];
-			this.delItem(i);
-		}
-	}
-};
-
-Furniture.prototype.move = function(x, y) {
-	if (this.dragItem &&
-		(x != this.dragItem.pos.x || y != this.dragItem.pos.y)) {
-		this.dragItem.setPosition({x: x, y: y});
-		return true;
-	}
-	return false;
-};
-
-Furniture.prototype.releaseItem = function() {
-	if (this.dragItem) {
-		this.addItem(this.dragItem);
-	}
-	this.dragItem = undefined;
-};
-
-Furniture.prototype.setDragItem = function(item) {
-	this.dragItem = item;
-};
-
 ////////////////////////////// svg
 var SvgFurniture = function(left, top, width, height, svg) {
 	this.svg = svg;
@@ -258,6 +168,7 @@ SvgFurniture.prototype.setWallHorizontal = function(x, y) {
 	if (!adj.left || !adj.right) {
 		this.dragItem.remove();
 		this.dragItem = undefined;
+		return;
 	}
 
 	var left = adj.left.opt.area.right;
--- js/main.js
+++ js/main.js
@@ -2,55 +2,6 @@ const WH_RATIO = (3/4);
 
 var Apl = function() {
 	this.dragging = false;
-
-	// get canvas DOM element and context
-	var $canvas = $('#canvas');
-	if ( ! $canvas[0] || ! $canvas[0].getContext ) {
-		return false;
-	}
-	this.ctx = $canvas[0].getContext("2d");
-
-	// resize canvas
-	$canvas.css('width', $canvas.width());
-	$canvas.css('height', $canvas.width()*WH_RATIO);
-	this.canvasStyleWidth = $canvas.width();
-	this.canvasStyleHeight = $canvas.height();
-
-	$canvas.attr('width', 1024);
-	$canvas.attr('height', 1024*WH_RATIO);
-
-	// get canvas info
-	this.canvasLeft = $canvas.offset().left;
-	this.canvasTop = $canvas.offset().top;
-	this.canvasWidth = parseInt($canvas.attr('width'));
-	this.canvasHeight = parseInt($canvas.attr('height'));
-
-	// context settnigs
-	this.ctx.strokeStyle = "#888";
-	this.ctx.lineWidth = 1;
-	this.ctx.globalCompositeOperation = "source-over";
-
-	// create room
-	this.furniture = new Furniture(0, 100, this.canvasWidth, this.canvasHeight - 100);
-
-	// create menu
-	this.menu = new Menu(function(item) {
-		this.furniture.setDragItem(item);
-		this.dragging = true;
-	}.bind(this));
-
-	// set events to the canvas
-	$canvas.mousedown(this.hDown.bind(this));
-	$canvas.mouseup(this.hUp.bind(this));
-	$canvas.mouseleave(this.hUp.bind(this));
-	$canvas.mousemove(this.hMove.bind(this));
-	$canvas.bind('touchstart', this.hDown.bind(this));
-	$canvas.bind('touchend', this.hUp.bind(this));
-	$canvas.bind('touchmove', this.hMove.bind(this));
-
-	this.draw();
-
-	////////////////////////////// svg
 	this.svg = Snap('#svg');
 
 	/*
@@ -111,30 +62,8 @@ var Apl = function() {
 	$('#svg').bind('touchmove', this.hMove.bind(this));
 };
 
-Apl.prototype._blank = function() {
-	this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-};
-
-Apl.prototype.draw = function() {
-	this._blank();
-	this.ctx.save();
-	this.furniture.draw(this.ctx);
-	this.ctx.restore();
-};
-
 Apl.prototype.hDown = function(e) {
 	if (!this.dragging) {
-		// convert coordinate from point to canvas
-		var x = (e.pageX? e.pageX: e.touches[0].pageX) - this.canvasLeft;
-		var y = (e.pageY? e.pageY: e.touches[0].pageY) - this.canvasTop;
-		x = parseInt(x * this.canvasWidth / this.canvasStyleWidth);
-		y = parseInt(y * this.canvasHeight / this.canvasStyleHeight);
-		if (this.furniture.isInternal(x, y)) {
-			this.furniture.grabItem(x, y);
-		}
-		this.dragging = true;
-
-		////////////////////////////// svg
 		var x = (e.pageX? e.pageX: e.touches[0].pageX);
 		var y = (e.pageY? e.pageY: e.touches[0].pageY);
 		var obj = Snap.getElementByPoint(x, y);
@@ -149,17 +78,13 @@ Apl.prototype.hDown = function(e) {
 
 Apl.prototype.hUp = function(e) {
 	this.dragging = false;
-	this.furniture.releaseItem();
-	this.draw();
-	e.preventDefault();
-////////////////////////////// svg
 	this.svgFurniture.releaseItem();
+	e.preventDefault();
 };
 
 Apl.prototype.hMove = function(e, p) {
 	if (this.dragging) {
-		// convert coordinate from point to canvas
-		var pageX, pageY, x, y;
+        var pageX, pageY, x, y;
 		if (e.pageX) {
 			pageX = e.pageX;
 			pageY = e.pageY;
@@ -170,15 +95,6 @@ Apl.prototype.hMove = function(e, p) {
 			pageX = p.pageX;
 			pageY = p.pageY;
 		}
-		x = pageX - this.canvasLeft;
-		y = pageY - this.canvasTop;
-		x = parseInt(x * this.canvasWidth / this.canvasStyleWidth);
-		y = parseInt(y * this.canvasHeight / this.canvasStyleHeight);
-		// check if the canvas should be updated
-		if (this.furniture.move(x, y)) {
-			this.draw();
-		}
-		////////////////////////////// svg
 		x = pageX - this.svgLeft;
 		y = pageY - this.svgTop;
 		x = parseInt(x * this.svgWidth / this.svgStyleWidth);
